b90a675ea9b8570dbade9b8f0a8ccd05b0a4b166
Gagan Sopori <gagan.sopori@hotmail.com>
2026-01-29 21:28:33 -0600

    Update __init__.py

--- source/__init__.py
+++ source/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
 
-def create_app(config=None):
-    app = Flask(__name__)
+@app.route("/")
+def welcome_page():
+    return render_template('index.html')
 
-    @app.route("/")
-    def welcome_page():
-        return render_template('index.html')
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
 
