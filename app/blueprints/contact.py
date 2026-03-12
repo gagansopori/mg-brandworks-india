06935e3cf6d6cfa1735ee826684db781daa0d84a
Gagan Sopori <gagan.sopori@hotmail.com>
2026-03-12 17:37:20 -0500

    Update contact.py

--- app/blueprints/contact.py
+++ app/blueprints/contact.py
@@ -1,30 +1,23 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_mail import Message
 # Import your ContactForm class here
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-from app.extensions import mail
+from app.email_service import EmailService
+from app.utils.extensions import mail
 
 contact_bp = Blueprint('contact', __name__)
 
 @contact_bp.route('/contact', methods=['GET', 'POST'])
 def index():
-    form = ContactForm()
-    if form.validate_on_submit():
-        msg = Message(subject=f"Inquiry from {form.name.data}",
-                      sender="your-email@gmail.com",
-                      recipients=["business@mgbrandworks.in"],
-                      body=form.message.data)
-        mail.send(msg)
+    data = request.form.to_dict()
+    service = EmailService(data)
+    success = service.send_email()
+    if success:
         flash('Message Sent!', 'success')
         return redirect(url_for('contact.index'))
-    return render_template('contact.html')
-
-
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = TextAreaField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send Message')
+    else:
+        flash('Failed to send message. Please try again...', 'danger')
+        return render_template('contact.html')
